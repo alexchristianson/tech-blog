4b511bf8f31a3417808d91b1dc7f641cb5c459e7
Alex C <alex.christianson.05@gmail.com>
2021-11-30 18:41:45 -0600

    fixed post update route

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -96,14 +96,13 @@ router.put('/:id', withAuth, (req, res) => {
     Post.update(
         {
             title: req.body.title,
-            text: req.body.post_text
+            post_text: req.body.post_text
         },
         {
             where: {
-                id: req.params.id
-            }
+            id: req.params.id
         }
-    )
+    })
     .then(dbPostData => {
         if (!dbPostData) {
             res.status(404).json({ message: 'No post found with this id' });
